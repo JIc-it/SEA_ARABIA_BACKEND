cfa3ef4f11c731acec3be7f43009ef469d7fd0e8
infanrahman <infanrahman4@gmail.com>
2023-12-13 12:55:17 +0530

    feat:offer urls update

--- local_apps/offer/urls.py
+++ local_apps/offer/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('offers/admin', AdminOfferListView.as_view(), name='offer-list-admin'),
     path('offers/application', ApplicationOfferListView.as_view(), name='offer-list-application'),
     path('offers/create/', OfferCreateView.as_view(), name='offer-create'),
-    path('offers/<int:pk>/', OfferRetrieveView.as_view(), name='offer-retrieve'),
-    path('offers/<int:pk>/update/', OfferUpdateView.as_view(), name='offer-update'),
+    path('offers/<uuid:pk>/', OfferRetrieveView.as_view(), name='offer-retrieve'),
+    path('offers/<uuid:pk>/update/', OfferUpdateView.as_view(), name='offer-update'),
 ]
