99db5c24368fe73b9cb6cbc2ac04e2ea0b8bee1a
Prince Rajendran <princerajendranofficial@gmail.com>
2023-12-28 12:25:14 +0530

    offer filter changed

--- local_apps/offer/filters.py
+++ local_apps/offer/filters.py
@@ -13,6 +13,17 @@ class OfferFilter(django_filters.FilterSet):
         lookup_expr='lte',
     )
 
+    is_enable = django_filters.Filter(
+        field_name='is_enable',
+        method='filter_is_enable'
+    )
+
+    def filter_is_enable(self, queryset, name, value):
+        data_true = True if value == 'True' or value == 'true' or value == True else None
+        data_false = False if value == 'False' or value == 'false' or value == False else None
+        value = data_true if data_true else data_false
+        return queryset.filter(**{name: value})
+
     class Meta:
         model = Offer
         fields = [
