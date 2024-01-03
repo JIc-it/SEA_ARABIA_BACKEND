6769eb0aa138d629a0da28ed21f9dabab0ea6156
infanrahman <infanrahman4@gmail.com>
2024-01-03 18:25:04 +0530

    feat:booking cancellation in booking app

--- local_apps/booking/models.py
+++ local_apps/booking/models.py
@@ -106,6 +106,8 @@ class Booking(Main):
     booking_type = models.CharField(
         choices=BOOKING_CHOICE, default='Booking', max_length=255, blank=True, null=True)
     cancellation_reason = models.TextField(blank=True, null=True)
+    canceld_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True,
+                             related_name='booking_canceld_user')
     refund_status = models.CharField(
         choices=REFUND_STATUS, default=None, max_length=255, blank=True, null=True)
     refund_type = models.CharField(
@@ -128,69 +130,75 @@ class Booking(Main):
     def __str__(self):
         return self.booking_id
 
-    # def save(self, *args, **kwargs):
-    #     temp_price = self.price.price if self.price and self.price.price else 0
-    #     offer_amount = 0
+    def save(self, *args, **kwargs):
+        if not self.offer:
+            super(Booking, self).save(*args, **kwargs)
+            return
+        temp_price = self.price.price if self.price and self.price.price else 0
+        offer_amount = 0
 
-    #     if self.booking_item in ['Activity', 'Service']:
-    #         redeem_temp = False
-    #         if self.offer.redemption_type in ['One-Time', 'Limited-Number']:
-    #             if self.offer.redemption_type == 'One-Time' and self.offer.redeem_count < 1:
-    #                 redeem_temp = True
-    #             elif self.offer.redemption_type == 'Limited-Number' and self.offer.specify_no >= self.offer.redeem_count:
-    #                 redeem_temp = True
-    #         else:
-    #             redeem_temp = True
-    #         if self.price and self.offer and redeem_temp:
-    #             if self.offer.discount_type == 'Percentage':
-    #                 offer_amount = (temp_price * self.offer.discount_value) / 100
-    #                 if offer_amount >= self.offer.up_to_amount:
-    #                     offer_amount = self.offer.up_to_amount
-    #             else:
-    #                 offer_amount = self.offer.discount_value
-    #             if self.offer.purchase_requirement and self.offer.min_purchase_amount <= temp_price:
-    #                 self.price_total = temp_price - offer_amount
-    #             else:
-    #                 self.price_total = temp_price
-    #         else:
-    #             self.price_total = self.price if self.price and self.price.price else 0
+        if self.booking_item in ['Activity', 'Service']:
+            redeem_temp = False
+            if self.offer.redemption_type in ['One-Time', 'Limited-Number']:
+                if self.offer.redemption_type == 'One-Time' and self.offer.redeem_count < 1:
+                    redeem_temp = True
+                elif self.offer.redemption_type == 'Limited-Number' and self.offer.specify_no >= self.offer.redeem_count:
+                    redeem_temp = True
+            else:
+                redeem_temp = True
+            if self.price and self.offer and redeem_temp:
+                if self.offer.discount_type == 'Percentage':
+                    offer_amount = (temp_price * self.offer.discount_value) / 100
+                    if offer_amount >= self.offer.up_to_amount:
+                        offer_amount = self.offer.up_to_amount
+                else:
+                    offer_amount = self.offer.discount_value
+                if self.offer.purchase_requirement and self.offer.min_purchase_amount <= temp_price:
+                    self.price_total = temp_price - offer_amount
+                else:
+                    self.price_total = temp_price
+            else:
+                self.price_total = self.price if self.price and self.price.price else 0
 
-    #         if self.offer.allow_multiple_redeem in ['One-Time', 'Multiple-Time']:
-    #             if self.offer.allow_multiple_redeem == 'One-Time' and self.offer.multiple_redeem_count < 1:
-    #                 redeem_temp = True
-    #             elif self.offer.allow_multiple_redeem == 'Multiple-Time' and self.offer.multiple_redeem_specify_no >= self.offer.multiple_redeem_count:
-    #                 redeem_temp = True
-    #         else:
-    #             redeem_temp = True
-    #             if not redeem_temp:
-    #                 raise ValidationError('You have already redeemed this Offer')
+            if self.offer.allow_multiple_redeem in ['One-Time', 'Multiple-Time']:
+                if self.offer.allow_multiple_redeem == 'One-Time' and self.offer.multiple_redeem_count < 1:
+                    redeem_temp = True
+                elif self.offer.allow_multiple_redeem == 'Multiple-Time' and self.offer.multiple_redeem_specify_no >= self.offer.multiple_redeem_count:
+                    redeem_temp = True
+                else:
+                    redeem_temp = True
+                    if not redeem_temp:
+                        raise ValidationError('You have already redeemed this Offer')
+            if self.offer.expiration in ['No-Expiry', 'Limited-Time']:
+                if self.offer.expiration == 'No-Expiry' and self.offer.start_date:
+                    redeem_temp = True
+                elif self.offer.expiration == 'Limited-Time' and self.offer.end_date:
+                    if self.offer.end_date >= date.today():
+                        redeem_temp = True
+                else:
+                    raise ValidationError("This Offer has expired")
+            else:
+                raise ValidationError("Invalid offer expiration")   
+            if self.service in self.offer.services.all():
+                redeem_temp=True
+            else:
+                raise ValidationError("The service provided does not match the service of this Offer.")
 
-    #         if self.offer.expiration in ['No-Expiry','Limited-Time']:
-    #             if self.offer.expiration == 'No-Expiry' and self.offer.start_date:
-    #                 redeem_temp=True
-    #             elif self.offer.expiration == 'Limited-Time' and self.offer.end_date >= date.today():
-    #                 redeem_temp=True
-    #             else:
-    #                 raise ValidationError("This Offer has expired")
-    #         if self.service in self.offer.services.all():
-    #             redeem_temp=True
-    #         else:
-    #             raise ValidationError("The service provided does not match the service of this Offer.")
-
-    #         if self.offer.purchase_requirement == True and self.price.price >= self.offer.min_purchase_amount:
-    #             redeem_temp=True
-
-    #         else:
-    #             redeem_temp= False
+            if self.offer.purchase_requirement == True and self.price.price >= self.offer.min_purchase_amount:
+                redeem_temp=True
+                
+            else: 
+                redeem_temp= False
 
     #     elif self.booking_item in ['Package', 'Event']:
     #         self.price_total = self.package.price if self.package and self.package.price else 0
     #     else:
     #         self.price_total = 0
 
-    #     if self.offer:
-    #         self.offer.redeem_count = self.offer.redeem_count + 1
+        if self.offer:
+            self.offer.redeem_count = self.offer.redeem_count + 1
 
-    #         self.offer.save()
+            self.offer.save()
 
-    #     super(Booking, self).save(*args, **kwargs)
+        super(Booking, self).save(*args, **kwargs)
+
