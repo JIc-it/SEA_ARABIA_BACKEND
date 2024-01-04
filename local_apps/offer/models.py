5568daa01f7dbb1cb9f229096223f8f0da2d8ca6
PranavSudhanan <pranavsudhanan98@gmail.com>
2024-01-04 11:40:16 +0530

    feat: implemented model logs in all apps

--- local_apps/offer/models.py
+++ local_apps/offer/models.py
@@ -1,4 +1,10 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.serializers import serialize
 from django.db import models
+from django.utils import timezone
+
+from local_apps.api_report.middleware import get_current_request
+from local_apps.api_report.models import ModelUpdateLog
 from local_apps.main.models import Main
 from rest_framework import serializers
 from local_apps.service.models import Service
@@ -48,6 +54,38 @@ class Offer(Main):
 
     def __str__(self):
         return self.name
+
+    def create_update_log(self, data_before, data_after):
+        request = get_current_request()
+        ModelUpdateLog.objects.create(
+            model_name=self.__class__.__name__,
+            user=request.user if request and hasattr(request, 'user') else None,
+            timestamp=timezone.now(),
+            data_before=data_before,
+            data_after=data_after
+        )
+
+    def save(self, *args, **kwargs):
+        # Check if the instance already exists
+        if self.pk:
+            try:
+                # Get the data before the update
+                data_before = serialize('json', [Offer.objects.get(pk=self.pk)])
+            except ObjectDoesNotExist:
+                # Instance doesn't exist yet, set data_before to None
+                data_before = None
+
+            # Call the original save method to save the instance
+            super(Offer, self).save(*args, **kwargs)
+
+            # Get the data after the update
+            data_after = serialize('json', [self])
+
+            # Create a log entry
+            self.create_update_log(data_before, data_after)
+        else:
+            # Call the original save method to save the instance
+            super(Offer, self).save(*args, **kwargs)
 
     class Meta:
         ordering = ["-created_at", "-updated_at"]
