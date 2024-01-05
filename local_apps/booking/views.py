bdbab0dfb700893dffa33aef6f098b3d7248fcd2
infanrahman <infanrahman4@gmail.com>
2024-01-05 10:55:22 +0530

    feat:serilizer changes view changes in booking app

--- local_apps/booking/views.py
+++ local_apps/booking/views.py
@@ -38,12 +38,26 @@ class AdminIndividualBookingView(generics.RetrieveAPIView):
 
     def get_object(self):
         try:
-            booking_id = self.request.data.get('booking_id', None)  
+            booking_id = self.request.data.get('booking_id', None)
+            if booking_id is None:
+                raise ValueError("Booking ID is required.")
+
             return Booking.objects.get(id=booking_id)
         except Booking.DoesNotExist:
             return Response({"error": "Booking not found"}, status=status.HTTP_404_NOT_FOUND)
+        except ValueError as ve:
+            return Response({"error": str(ve)}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
-            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)    
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        if isinstance(instance, Response):
+            return instance
+
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)   
 
 
 class VendorBookingListView(generics.ListAPIView):
